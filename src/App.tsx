9831edd48e735c1f1ecc032c2bd5b33ded8282f6
Kabir Kashif <kashif.kabir60@gmail.com>
2025-01-16 11:11:43 +0500

    playing with NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+import AllRoutes from "./routes/AllRoutes"
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      Hello World
-    </div>
+    <BrowserRouter>
+      <AllRoutes />
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      Error 404. Page Not Found <span>🥲</span>
+    </div>
+  )
+}
